fabec3975fdff588accae1f3c98e17b5d4474ce9
Okakakai <inoue.kai.work@gmail.com>
2022-11-26 11:51:45 +0900

    add p comp has state owership is weak perfermance

---
+++ pages/temp2.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Input({ value, handleChange }) {
+    return <input value={value} onChange={e => handleChange(e.target.value)} placeholder="Temp in °C"/>;
+  }
+
+//簡単な解決法
+//小要素で共有する state 情報をただ親に持たせただけ
+//しかし、setvalue , Inputが受け取る値が変わるたびに全てが再レンダリングするため、パフォーマンスが悪くなる
+export default function Temp2() {
+    const [value, setValue] = useState("");
+
+    return (
+        <div className="Home">
+            <h1> Temperature Converter </h1>
+            <Input value={value} handleChange={setValue}/>
+            <Kelvin value={value}/>
+            <Fahrenheit value={value}/>
+        </div>
+    );
+}
+
+function Kelvin({ value = 0 }){
+  return <div className="temp">{value + 273.15}K</div>
+}
+
+function Fahrenheit({ value = 0}){
+  return <div className="temp">{(value * 9) / 5 + 32}°F</div>
+}
