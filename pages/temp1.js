101d0e985eb6e616fe9eebef365377b89e4a9f49
Okakakai <inoue.kai.work@gmail.com>
2022-11-26 11:40:59 +0900

    add did not use render props so input is gomi

---
+++ pages/temp1.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Input() {
+  const [value, setValue] = useState("");
+
+  return (
+    <input type={"text"} value={value} onChange={e => setValue(e.target.value)} placeholder="Temp in °C"/>
+  );
+}
+
+export default function Temp1() {
+  return (
+    <div className="Home">
+      <h1> Temperature Converter </h1>
+      <Input />
+      <Kelvin />
+      <Fahrenheit />
+    </div>
+  );
+}
+
+function Kelvin({ value = 0 }){
+  return <div className="temp">{value + 273.15}K</div>
+}
+
+function Fahrenheit({ value = 0}){
+  return <div className="temp">{(value * 9) / 5 + 32}°F</div>
+}
